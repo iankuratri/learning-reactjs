78151507d0f8b6c08d5ac5c9883194f5b28c8d9a
Ankur Atri <ankur@zenways.io>
2020-01-21 19:54:55 +0530

    code aaded to component

---
+++ 2-counter-app/src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      // wrap the code inside a div or React.Fragment
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
