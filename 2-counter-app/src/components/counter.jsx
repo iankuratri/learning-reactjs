634188c954131424a26492c1541684789f13e4f7
Ankur Atri <ankur@zenways.io>
2020-01-22 12:26:17 +0530

    minor code added

--- 2-counter-app/src/components/counter.jsx
+++ 2-counter-app/src/components/counter.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       // wrap the code inside a div or React.Fragment
       <div>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
