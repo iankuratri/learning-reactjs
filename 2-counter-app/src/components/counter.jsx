7829319b21e9dd47b3dcf04896c20248e8aa32b7
Ankur Atri <ankur@zenways.io>
2020-01-23 14:11:39 +0530

    condtional and structural rendering

--- 2-counter-app/src/components/counter.jsx
+++ 2-counter-app/src/components/counter.jsx
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
-    // imageUrl: "https://picsum.photos/200"
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+
+    // for rendering tags
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold"
-  };
+  // styles = {
+  //   fontSize: 50,
+  //   fontWeight: "bold"
+  // };
+
+  renderTages() {
+    if (!this.state.tags.length) return <p>There are not tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
@@ -17,12 +31,27 @@ class Counter extends Component {
       <div>
         {/* <img src={this.state.imageUrl} /> */}
         {/* or oyu can use inline styles like that style={ {fontSize: 50, fontWeight: "bold"} } */}
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        {/* style={this.styles} */}
+
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+
+        {!this.state.tags.length && "Please create a new tag!"}
+        {this.renderTages()}
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
