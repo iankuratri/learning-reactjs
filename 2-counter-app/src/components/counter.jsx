1363a14b23f9687223dce4694125ee83b8458890
Ankur Atri <ankur@zenways.io>
2020-01-23 12:56:42 +0530

    applying styles

--- 2-counter-app/src/components/counter.jsx
+++ 2-counter-app/src/components/counter.jsx
@@ -3,14 +3,24 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0
+    // imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
   };
 
   render() {
     return (
       // wrap the code inside a div or React.Fragment
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} /> */}
+        {/* or oyu can use inline styles like that style={ {fontSize: 50, fontWeight: "bold"} } */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
