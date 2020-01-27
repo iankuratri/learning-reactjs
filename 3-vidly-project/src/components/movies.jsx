3c1f15d8fae2818515dfdc0cd9a7918285d743ca
Ankur Atri <ankur@zenways.io>
2020-01-27 13:21:20 +0530

    delete function added

--- 3-vidly-project/src/components/movies.jsx
+++ 3-vidly-project/src/components/movies.jsx
@@ -5,24 +5,42 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   };
+
+  deleteMovie = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
   render() {
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">Title</th>
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
+            <th scope="col" className="text-center">
+              Action
+            </th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td className="text-center">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => this.deleteMovie(movie)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
