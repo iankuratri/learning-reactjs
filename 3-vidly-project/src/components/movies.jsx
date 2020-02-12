1f702960b0cc90da389375eadbacc6d05539e433
Ankur Atri <ankur@zenways.io>
2020-02-12 13:36:32 +0530

    common folder added

--- 3-vidly-project/src/components/movies.jsx
+++ 3-vidly-project/src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "./../services/fakeMovieService";
-import Like from "./like";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
