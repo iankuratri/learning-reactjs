c2978a7328ec7dd789f239c1fb18f774e4874f1f
Ankur Atri <ankur@zenways.io>
2020-02-24 14:11:57 +0530

    simple input field error validation done

--- 3-vidly-project/src/components/loginForm.jsx
+++ 3-vidly-project/src/components/loginForm.jsx
@@ -29,21 +29,36 @@ class LoginForm extends Component {
 
     const errors = this.validate();
     console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     console.log("submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
 
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -55,12 +70,14 @@ class LoginForm extends Component {
             name="username"
             value={account.username}
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             label="Password"
             name="password"
             value={account.password}
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary mt-3">Login</button>
         </form>
