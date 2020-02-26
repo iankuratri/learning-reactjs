0e8564c1ac9d26aac9c624accecea0ad9f6c3062
Ankur Atri <ankur@zenways.io>
2020-02-26 13:19:49 +0530

    register form added

--- 3-vidly-project/src/components/registerForm.jsx
+++ 3-vidly-project/src/components/registerForm.jsx
@@ -1,20 +1,25 @@
+import Joi from "joi-browser";
 import React from "react";
 import Form from "./common/form";
-import Joi from "joi-browser";
 
-class LoginForm extends Form {
+class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: "", password: "", name: "" },
     errors: {}
   };
 
   schema = {
     username: Joi.string()
+      .email()
       .required()
       .label("Username"),
     password: Joi.string()
+      .min(5)
       .required()
-      .label("Password")
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
   };
 
   doSubmit = () => {
@@ -24,16 +29,17 @@ class LoginForm extends Form {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <h1>Register</h1>
 
         <form className="mt-5" onSubmit={this.handleSubmit}>
           {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
         </form>
       </div>
     );
   }
 }
 
-export default LoginForm;
+export default RegisterForm;
