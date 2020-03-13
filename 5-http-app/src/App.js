c0437636da9472898b035ff5d1d7bb42296110ae
Ankur Atri <ankuratri@hotmail.com>
2020-03-13 23:25:36 +0530

    optimistic updates implimented

--- 5-http-app/src/App.js
+++ 5-http-app/src/App.js
@@ -35,10 +35,18 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(`${apiEndpoint}/${post.id}`);
+    const originalPosts = this.state.posts;
 
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(`${apiEndpoint}/${post.id}`);
+      // throw new Error("");
+    } catch (ex) {
+      alert("Something went wrong when deleting the post.");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
