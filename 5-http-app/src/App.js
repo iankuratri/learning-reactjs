3c90b47f94fe6a79df575c0655cbd8d1649191ab
Ankur Atri <ankuratri@hotmail.com>
2020-03-13 23:18:29 +0530

    api's integrated

--- 5-http-app/src/App.js
+++ 5-http-app/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
+
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
     posts: []
   };
 
-  handleAdd = () => {
-    console.log("Add");
+  async componentDidMount() {
+    const { data: posts } = await axios.get(apiEndpoint);
+
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const obj = { title: "new post", body: "this is my new post" };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
-  handleUpdate = post => {
-    console.log("Update", post);
+  handleUpdate = async post => {
+    post.title = "Updated post";
+
+    await axios.put(`${apiEndpoint}/${post.id}`, post);
+
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
   };
 
-  handleDelete = post => {
-    console.log("Delete", post);
+  handleDelete = async post => {
+    await axios.delete(`${apiEndpoint}/${post.id}`);
+
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
   };
 
   render() {
