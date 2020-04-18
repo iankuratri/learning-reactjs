ee085e3c2b0652d804ae5a29010a81e0559d9d46
Ankur Atri <ankuratri@hotmail.com>
2020-04-18 15:50:52 +0530

    issue catcher done

--- 5-http-app/src/index.js
+++ 5-http-app/src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-// import logger from "./services/logService";
-import * as Sentry from "@sentry/browser";
+import logger from "./services/logService";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Sentry.init({
-  dsn:
-    "https://be95b3df8d0c4e969a9453764cdddab0@o379702.ingest.sentry.io/5204872",
-});
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
