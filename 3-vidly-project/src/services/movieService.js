ac48d62a37b86fcdedad9c4ddf866b01faeb23da
Ankur Atri <ankuratri@hotmail.com>
2020-04-19 18:11:49 +0530

    minor code refactor

--- 3-vidly-project/src/services/movieService.js
+++ 3-vidly-project/src/services/movieService.js
@@ -3,23 +3,27 @@ import { apiUrl } from "./../config.json";
 
 const apiEndPoint = `${apiUrl}/movies`;
 
+function movieUrl(id) {
+  return `${apiEndPoint}/${id}`;
+}
+
 export function getMovies() {
   return _http.get(apiEndPoint);
 }
 
 export function deleteMovie(movieId) {
-  return _http.delete(`${apiEndPoint}/${movieId}`);
+  return _http.delete(movieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-  return _http.get(`${apiEndPoint}/${movieId}`);
+  return _http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return _http.put(`${apiEndPoint}/${movie._id}`, body);
+    return _http.put(movieUrl(movie._id), body);
   }
 
   return _http.post(apiEndPoint, movie);
