9e0d5b0054cf734ebf840612b67e35b4fb30b686
Ankur Atri <ankuratri@hotmail.com>
2020-04-22 23:49:37 +0530

    auth and dynamic render acc to user roles done

--- 3-vidly-project/src/components/loginForm.jsx
+++ 3-vidly-project/src/components/loginForm.jsx
@@ -19,7 +19,9 @@ class LoginForm extends Form {
       const { username: email, password } = this.state.data;
       await auth.login({ email, password });
 
-      window.location = "/";
+      const { state } = this.props.location;
+
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
--- 3-vidly-project/src/components/moviesTable.jsx
+++ 3-vidly-project/src/components/moviesTable.jsx
@@ -2,36 +2,49 @@ import React, { Component } from "react";
 import Table from "./common/table";
 import Like from "./common/like";
 import { Link } from "react-router-dom";
+import auth from "./../services/authService";
 
 class MoviesTable extends Component {
   columns = [
     {
       label: "Title",
       path: "title",
-      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
     },
     { label: "Genre", path: "genre.name" },
     { label: "Stock", path: "numberInStock" },
     { label: "Rate", path: "dailyRentalRate" },
     {
       key: "like",
-      content: movie => (
+      content: (movie) => (
         <Like onClick={() => this.props.onLike(movie)} isLiked={movie.liked} />
-      )
+      ),
     },
-    {
-      key: "delete",
-      content: movie => (
-        <button
-          type="button"
-          className="btn btn-danger"
-          onClick={() => this.props.onDelete(movie)}
-        >
-          Delete
-        </button>
-      )
-    }
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => this.props.onDelete(movie)}
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
