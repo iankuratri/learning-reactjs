fcce84c3b836d2e5919456aef4a27aadee348302
Ankur Atri <ankur@zenways.io>
2022-01-21 22:50:42 +0530

    No Match Found route added

--- 8-router-v6/src/App.js
+++ 8-router-v6/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
