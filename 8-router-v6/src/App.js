b43d06f90de98f29fe1de5515bc2bfed6c47a29e
Ankur Atri <ankur@zenways.io>
2022-01-21 22:09:25 +0530

    Links added

--- 8-router-v6/src/App.js
+++ 8-router-v6/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
