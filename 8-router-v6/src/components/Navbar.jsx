e24b43aa6ab5f3307957d23adad9d413798c870d
Ankur Atri <ankur@zenways.io>
2022-01-21 22:19:40 +0530

    active links added

--- 8-router-v6/src/components/Navbar.jsx
+++ 8-router-v6/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
   );
 };
