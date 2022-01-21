fd3dc697bee67561d1b84e4c3e9ff9a36b80cf56
Ankur Atri <ankur@zenways.io>
2022-01-21 22:41:42 +0530

    Navigating Programmatically

--- 8-router-v6/src/App.js
+++ 8-router-v6/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
       </Routes>
     </>
   );
--- 8-router-v6/src/components/Home.jsx
+++ 8-router-v6/src/components/Home.jsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div>Home Page</div>
+      {/* to replace history use -> navigate("order-summary", { replace: true }) */}
+      <button onClick={() => navigate("order-summary")}>Place order</button>
+    </>
+  );
 };
 
 export default Home;
