33167895a237b97cba10a8defb9a5452267a921b
Ankur Atri <ankuratri.in@gmail.com>
2022-01-30 23:06:07 +0530

    react-icons added

--- 9-practical-react/src/components/Icons.jsx
+++ 9-practical-react/src/components/Icons.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { FaAngellist } from "react-icons/fa";
+import { AiFillFormatPainter } from "react-icons/ai";
+import { IconContext } from "react-icons/lib";
+import { FcBusiness } from "react-icons/fc";
 
 export const Icons = () => {
-  return <h1>This is icon component.</h1>;
+  return (
+    <IconContext.Provider value={{ color: "green", size: "5rem" }}>
+      <FaAngellist />
+      <AiFillFormatPainter color="red" size="4rem" />
+      <FcBusiness size="3rem" />
+    </IconContext.Provider>
+  );
 };
