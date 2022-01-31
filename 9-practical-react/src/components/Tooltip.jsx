05a16ee81b197b865c8b9ea9b13075fa2af0f74a
Ankur Atri <ankuratri.in@gmail.com>
2022-01-31 12:18:34 +0530

    tippy and its examples added

--- 9-practical-react/src/components/Tooltip.jsx
+++ 9-practical-react/src/components/Tooltip.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "tippy.js/dist/tippy.css";
+import Tippy from "@tippyjs/react";
+
+const ColoredComponent = () => {
+  return <div style={{ color: "gold" }}>Component Tooltip</div>;
+};
 
 export const Tooltip = () => {
-  return <div>Tooltip component</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
+      <Tippy content="Basic Tooltip">
+        <button>Toottip - text</button>
+      </Tippy>
+
+      <Tippy
+        content={<h2 style={{ color: "dodgerblue" }}>This is heading 2</h2>}
+      >
+        <button>Tooltip - h2</button>
+      </Tippy>
+
+      <Tippy content={<ColoredComponent />}>
+        <button>Tooltip - component</button>
+      </Tippy>
+    </div>
+  );
 };
