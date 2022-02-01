e9c1a5281cabb584be3edeff2435a61883f8f49a
Ankur Atri <ankuratri.in@gmail.com>
2022-02-01 21:15:49 +0530

    tippyjs and its examples added

--- 9-practical-react/src/components/Tooltip.jsx
+++ 9-practical-react/src/components/Tooltip.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "tippy.js/dist/tippy.css";
 import Tippy from "@tippyjs/react";
 
@@ -6,14 +6,24 @@ const ColoredComponent = () => {
   return <div style={{ color: "gold" }}>Component Tooltip</div>;
 };
 
+const CustomChild = forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <h1>Heading 1</h1>
+      <p>Paragraph</p>
+    </div>
+  );
+});
+
 export const Tooltip = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
-      <Tippy content="Basic Tooltip">
-        <button>Toottip - text</button>
+      <Tippy arrow={false} delay={1000} content="Basic Tooltip">
+        <button>Tootip - text</button>
       </Tippy>
 
       <Tippy
+        placement="right"
         content={<h2 style={{ color: "dodgerblue" }}>This is heading 2</h2>}
       >
         <button>Tooltip - h2</button>
@@ -21,6 +31,10 @@ export const Tooltip = () => {
 
       <Tippy content={<ColoredComponent />}>
         <button>Tooltip - component</button>
+      </Tippy>
+
+      <Tippy content="Tooltip - custom child">
+        <CustomChild />
       </Tippy>
     </div>
   );
