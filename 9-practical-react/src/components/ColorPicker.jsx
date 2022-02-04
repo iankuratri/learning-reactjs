0e22ee90e9ceb1c1ca86dc2b919e67e5e62af323
Ankur Atri <ankuratri.in@gmail.com>
2022-02-04 22:16:47 +0530

    react-color and its examples added

--- 9-practical-react/src/components/ColorPicker.jsx
+++ 9-practical-react/src/components/ColorPicker.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChromePicker } from "react-color";
 
 export const ColorPicker = () => {
-  return <div>Color Picker component</div>;
+  const [color, setColor] = useState("#fffff");
+  const [showColorPicker, setShowColorPicker] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          setShowColorPicker((showColorPicker) => !showColorPicker)
+        }
+      >
+        {showColorPicker ? "Close color picker" : "Pick a color"}
+      </button>
+
+      {showColorPicker && (
+        <ChromePicker
+          color={color}
+          onChange={(updatedColor) => setColor(updatedColor.hex)}
+        />
+      )}
+
+      <h2>You picked {color}</h2>
+    </div>
+  );
 };
