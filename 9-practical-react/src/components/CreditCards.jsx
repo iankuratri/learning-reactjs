38e3bd8b2b42023c19e65608375c31a2ef042585
Ankur Atri <ankuratri.in@gmail.com>
2022-02-05 21:58:03 +0530

    react-credit-cards and its example added

--- 9-practical-react/src/components/CreditCards.jsx
+++ 9-practical-react/src/components/CreditCards.jsx
@@ -1,5 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import "@snowpak/react-credit-cards/es/styles-compiled.css";
+import Cards from "@snowpak/react-credit-cards";
 
 export const CreditCards = () => {
-  return <div>Credit Cards component</div>;
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focus, setFocus] = useState("");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "40px",
+        alignItems: "center",
+      }}
+    >
+      <Cards
+        cvc={cvc}
+        expiry={expiry}
+        focused={focus}
+        name={name}
+        number={number}
+      />
+
+      <form style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+        <input
+          className="form-control"
+          type="tel"
+          name="number"
+          maxLength={16}
+          placeholder="Card Number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+
+        <input
+          className="form-control"
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+
+        <div style={{ display: "flex", gap: "20px" }}>
+          <input
+            className="form-control"
+            type="text"
+            name="expiry"
+            maxLength={4}
+            placeholder="MM/YY Expiry"
+            value={expiry}
+            onChange={(e) => setExpiry(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+
+          <input
+            className="form-control"
+            type="tel"
+            name="cvc"
+            maxLength={3}
+            placeholder="CVC"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
