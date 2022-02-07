d8a6dfa16d27631eb57d1f8bbcef3273bcf0a02a
Ankur Atri <ankuratri.in@gmail.com>
2022-02-07 22:01:48 +0530

    react-spinners and its examples added

--- 9-practical-react/src/components/LoadingIndicators.jsx
+++ 9-practical-react/src/components/LoadingIndicators.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { BeatLoader, BounceLoader, PacmanLoader } from "react-spinners";
 
 export const LoadingIndicators = () => {
-  return <div>Loading Indicators component</div>;
+  return (
+    <div>
+      <BounceLoader size={24} color="red" />
+      <BeatLoader size={72} color="green" />
+      <PacmanLoader size={48} color="yellow" />
+    </div>
+  );
 };
