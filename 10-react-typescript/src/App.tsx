21be80ef55519a4ff90bd26d929666c12c3e3c65
Ankur Atri <ankuratri.in@gmail.com>
2022-02-12 21:36:56 +0530

    useContext Future Value and their types

--- 10-react-typescript/src/App.tsx
+++ 10-react-typescript/src/App.tsx
@@ -4,6 +4,8 @@ import { Button } from "./components/Button";
 import { Container } from "./components/Container";
 import { Box } from "./components/context/Box";
 import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { User } from "./components/context/User";
+import { UserContextProvider } from "./components/context/UserContext";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
@@ -64,11 +66,15 @@ function App() {
       <Container styles={{ border: "1px solid black", padding: "1rem" }} />
 
       <Counter />
-      */}
 
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
+      */}
+
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
---
+++ 10-react-typescript/src/components/context/UserContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+type AuthUser = {
+  name: string;
+  email: string;
+};
+
+type UserContextType = {
+  user: AuthUser | null;
+  setUser: React.Dispatch<React.SetStateAction<AuthUser | null>>;
+};
+
+type UserContextProviderProps = {
+  children: React.ReactChild;
+};
+
+export const UserContext = createContext({} as UserContextType);
+
+export const UserContextProvider = ({ children }: UserContextProviderProps) => {
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
