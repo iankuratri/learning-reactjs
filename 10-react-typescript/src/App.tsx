453f9c882fa9f8a5346812f50b2f00a788f56f66
Ankur Atri <ankuratri.in@gmail.com>
2022-02-19 00:04:01 +0530

    types in Polymorphic Components

--- 10-react-typescript/src/App.tsx
+++ 10-react-typescript/src/App.tsx
@@ -23,6 +23,7 @@ import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import { Toast } from "./components/template-literals/Toast";
 import { CustomButton } from "./components/html/Button";
+import { Text } from "./components/polymorphic/Text";
 
 function App() {
   const personName = {
@@ -102,11 +103,21 @@ function App() {
       <RandomNumber value={10} isPositive />
 
       <Toast position="center-bottom" />
-      */}
 
       <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
         Primary Button
       </CustomButton>
+      */}
+
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
