6f19ea8262d763930d85ff9a7a38648b161afe03
Ankur Atri <ankuratri.in@gmail.com>
2022-03-15 14:07:14 +0530

    basic project setup with navigation bar and home component

---
+++ 11-material-ui/src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
