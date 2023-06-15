65254d01c5ff103ea9a9a70cf58a283d2b1c5158
Ankur Atri <ankuratri.in@gmail.com>
2023-06-15 21:39:25 +0530

    Link tag and navigate fn example added

--- 12-react-query-zustang-router/src/routing/HomePage.tsx
+++ 12-react-query-zustang-router/src/routing/HomePage.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
-      <a href="/users">Users</a>
+      <Link to="/users">Users</Link>
+      <br />
+      <Link to="/contact">Contact</Link>
     </>
   );
 };
