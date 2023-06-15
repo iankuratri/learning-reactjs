629d426e5d4ae0ff4a37d5d58c881c9362b0c145
Ankur Atri <ankuratri.in@gmail.com>
2023-06-15 22:19:35 +0530

    route error fallback added

--- 12-react-query-zustang-router/src/routing/routes.tsx
+++ 12-react-query-zustang-router/src/routing/routes.tsx
@@ -4,11 +4,13 @@ import UsersPage from "./UsersPage";
 import ContactPage from "./ContactPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
